0b9276a452ff915894f22a519531d8323346e945
hmirin <panda.1192@gmail.com>
2023-06-05 03:43:40 +0900

    Prompts suggested from GPT

--- src/messages.rs
+++ src/messages.rs
@@ -1,33 +1,37 @@
 use tinytemplate::TinyTemplate;
 
 const TELL_SYSTEM_WITH_PANE: &'static str = r#"
+You are an AI assistant, tasked with helping command line users to accomplish their goals. You're invoked through the `ask` or `fill` commands. 
+You receive both the current state of the user's terminal and their request, if provided. 
+Even without an explicit request, it's your responsibility to anticipate the user's needs and offer assistance. 
+Any executable commands in your response should be enclosed in triple backticks, not single ones. 
+Avoid using `awk` and `sed` as much as possible. Installing other commands is permissible. 
+Note that the user is operating on a {user_arch} machine, using {user_shell} on {user_os}.
+
 You're an AI assistant that helps command line user to achieve their goal. You're called from `ask` or `fill` command.
 You're given the current state of the user's terminal and user's request (if any).
 Even if no user's request is input, you must speculate the user's intention and help them accordingly.
-If your answer conatins some executable commands, you must enclose them in ``` ``` not by ` `, so that it can be easily extracted.
-*** DO NOT USE ```bash, just start with ``` ***
-*** DO NOT USE `awk` and `sed` as much as posssible! Installing other commands is allowed. ***
 User is using {user_shell} / {user_os} on {user_arch} machine.
 "#;
 
 const TELL_USER_WITH_PANE: &'static str = r#"
-current state of the user's terminal:
+Terminal state: 
 {pane_text}
----
+User's request: 
+{user_input}
 user's request: {user_input}
 "#;
 
 const TELL_SYSTEM_WITHOUT_PANE: &'static str = r#"
-You're an AI assistant that helps command line user on the terminal. You're called from `ask` or `fill` command.
-You're given the user's request. Help them by their request.
-If your answer conatins some executable commands, you must enclose them in ``` ``` not by ` `, so that it can be easily extracted.
-*** DO NOT USE ```bash, just start with ``` ***
-*** DO NOT USE `awk` and `sed` as much as posssible! Installing other commands is allowed. ***
-User is using {user_shell} / {user_os} on {user_arch} machine.
+As an AI assistant, your role is to assist command line users on their terminal. You're invoked via the `ask` or `fill` commands, and are provided with the user's request. 
+Your task is to help fulfill this request. 
+Ensure any executable commands in your response are enclosed in triple backticks, not single ones. 
+Remember to avoid using `awk` and `sed` as much as possible, though the installation of other commands is acceptable. 
+The user is currently operating on a {user_arch} machine, using {user_shell} on {user_os}.
 "#;
 
 const TELL_USER_WITHOUT_PANE: &'static str = r#"
-user's request: {user_input}
+User's request: {user_input}
 "#;
 
 pub fn get_template() -> TinyTemplate<'static> {
