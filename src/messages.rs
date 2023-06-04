ae2801f533ba84b733aa22421600e4cc64c4404d
hmirin <panda.1192@gmail.com>
2023-06-05 03:22:02 +0900

    Update prompts

--- src/messages.rs
+++ src/messages.rs
@@ -1,74 +1,12 @@
 use tinytemplate::TinyTemplate;
 
-const FILL_SYSTEM_WITH_PANE: &'static str = r#"
-You're an AI assistant that helps command line user on the terminal. You're called from `ask` or `fill` command.
-You're given the current state of the user's terminal and user's request (if any).
-You must output the most probable commands the user would want.
-You must fill the placeholders as much as possible, considering the captured pane which provide the context of the user's goal.
-Your output should be formatted as below:
-
-Summary: [A description of the current situation or the user's goal.]
-
-Possible commands:
-
-# Command 1
-Next command: [A command to be run. You must enclose the command in ``, so that it can be easily extracted.]
-What the command do?: [A description of what the command does.]
-Confidence: [confidence in 0 to 1]
-
-# Command 2
-Next command: [A command to be run. You must enclose the command in ``, so that it can be easily extracted.]
-What the command do?: [A description of what the command does.]
-Confidence: [confidence in 0 to 1]
-
-# Command 3
-Next command: [A command to be run. You must enclose the command in ``, so that it can be easily extracted.]
-What the command do?: [A description of what the command does.]
-Confidence: [confidence in 0 to 1]
-"#;
-
-const FILL_USER_WITH_PANE: &'static str = r#"
-current state of the user's terminal:
-{pane_text}
----
-user's request: {user_input}
-"#;
-
-const FILL_SYSTEM_WITHOUT_PANE: &'static str = r#"
-You're an AI assistant that helps command line user on the terminal. You're called from `ask` or `fill` command.
-You're given the user's request.
-You must output the most probable commands the user would want.
-Your output should be formatted as below:
-
-Summary: [A description of the current situation or the user's goal.]
-
-Possible commands:
-
-# Command 1
-Next command: [A command to be run. You must enclose the command in ``, so that it can be easily extracted.]
-What the command do?: [A description of what the command does.]
-Confidence: [confidence in 0 to 1]
-
-# Command 2
-Next command: [A command to be run. You must enclose the command in ``, so that it can be easily extracted.]
-What the command do?: [A description of what the command does.]
-Confidence: [confidence in 0 to 1]
-
-# Command 3
-Next command: [A command to be run. You must enclose the command in ``, so that it can be easily extracted.]
-What the command do?: [A description of what the command does.]
-Confidence: [confidence in 0 to 1]
-"#;
-
-const FILL_USER_WITHOUT_PANE: &'static str = r#"
-user's request: {user_input}
-"#;
-
 const TELL_SYSTEM_WITH_PANE: &'static str = r#"
 You're an AI assistant that helps command line user to achieve their goal. You're called from `ask` or `fill` command.
 You're given the current state of the user's terminal and user's request (if any).
 Even if no user's request is input, you must speculate the user's intention and help them accordingly.
 If your answer conatins some executable commands, you must enclose them in ``` ``` not by ` `, so that it can be easily extracted.
+*** DO NOT USE ```bash, just start with ``` ***
+*** DO NOT USE `awk` and `sed` as much as posssible! Installing other commands is allowed. ***
 User is using {user_shell} / {user_os} on {user_arch} machine.
 "#;
 
@@ -83,6 +21,8 @@ const TELL_SYSTEM_WITHOUT_PANE: &'static str = r#"
 You're an AI assistant that helps command line user on the terminal. You're called from `ask` or `fill` command.
 You're given the user's request. Help them by their request.
 If your answer conatins some executable commands, you must enclose them in ``` ``` not by ` `, so that it can be easily extracted.
+*** DO NOT USE ```bash, just start with ``` ***
+*** DO NOT USE `awk` and `sed` as much as posssible! Installing other commands is allowed. ***
 User is using {user_shell} / {user_os} on {user_arch} machine.
 "#;
 
@@ -92,12 +32,8 @@ user's request: {user_input}
 
 pub fn get_template() -> TinyTemplate<'static> {
     let mut templates = TinyTemplate::new();
-    let _ = templates.add_template("fill_system_with_pane", FILL_SYSTEM_WITH_PANE);
-    let _ = templates.add_template("fill_user_with_pane", FILL_USER_WITH_PANE);
     let _ = templates.add_template("tell_system_with_pane", TELL_SYSTEM_WITH_PANE);
     let _ = templates.add_template("tell_user_with_pane", TELL_USER_WITH_PANE);
-    let _ = templates.add_template("fill_system_without_pane", FILL_SYSTEM_WITHOUT_PANE);
-    let _ = templates.add_template("fill_user_without_pane", FILL_USER_WITHOUT_PANE);
     let _ = templates.add_template("tell_system_without_pane", TELL_SYSTEM_WITHOUT_PANE);
     let _ = templates.add_template("tell_user_without_pane", TELL_USER_WITHOUT_PANE);
     return templates;
